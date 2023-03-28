28a07703a5c89b00f9dd77210b4a816a93cba574
PC1-IFLAB <44150851+aryasatya05@users.noreply.github.com>
2023-03-28 17:09:46 +0700

    last commit

--- jurnal6_1302210082/Program.cs
+++ jurnal6_1302210082/Program.cs
@@ -1,2 +1,90 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using System.Diagnostics.Contracts;
+
+internal class Program
+{
+    private static void Main(String[] args)
+    {
+        SayaTubeVideo video = new SayaTubeVideo("blalalalala");
+        video.PrintVidioDetail();
+        video.increasePlayCount(123);
+        video.PrintVidioDetail();
+
+        for(int i = 0;i < 100000000;)
+        {
+            video.increasePlayCount(i);
+            i = i + 100000000;
+        }
+    }
+}
+
+class SayaTubeUser
+{
+    private int id;
+    private List<SayaTubeVideo> uploadVideos;
+    private String username;
+
+    public SayaTubeUser(String username)
+    {
+        Random random = new Random(DateTime.Now.Minute);
+        this.username = username;
+
+    }
+
+    public int GetTotalVideoPlayCount()
+    {
+        return new int();
+    }
+
+    public void addVideo(SayaTubeVideo video)
+    {
+
+    }
+
+    public void PrintAllVideoPlayCount()
+    {
+
+    }
+}
+
+class SayaTubeVideo
+{
+    private int id;
+    private string title;
+    private int playCount;
+
+    public SayaTubeVideo(string title)
+    {
+        Contract.Requires(title.Length == 100 && title != null);
+
+        this.title = title;
+        this.playCount = 0;
+        Random random = new Random();
+        this.id = random.Next(100000);
+    }
+
+    public void increasePlayCount(int playCount)
+    {
+        Contract.Requires(playCount <= 100000);
+        try
+        {
+            checked
+            {
+                this.playCount = this.playCount + playCount;
+            }
+        }
+        catch(OverflowException overexcept)
+        {
+            Console.WriteLine("Penambahan playcount melebihi batas! " + overexcept);
+        }
+    }
+    
+    public void PrintVidioDetail()
+    {
+        Console.WriteLine("Judul video" + this.title + ",Jumlah diputar" + this.playCount + "id" + this.id);
+    }
+}
+
+
+
